5f026e96997ae96c4af30e800ae58cf99a1577e7
Jake Romer <mail@jakeromer.com>
2020-06-29 18:00:51 -0400

    Add dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js2-mode . ((flycheck-disabled-checkers . (javascript-standard))))
+ (rjsx-mode . ((flycheck-disabled-checkers . (javascript-standard))))))
