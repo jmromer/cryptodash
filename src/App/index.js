68c6e9b1ca7f34db24b99fd7ef38dc0a04fa6004
Jake Romer <mail@jakeromer.com>
2020-06-29 18:01:11 -0400

    Add AppLayout using styled comps

--- src/App/index.js
+++ src/App/index.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import './App.css';
-import styled, { css } from 'styled-components';
+import AppLayout from './AppLayout';
 
 import WelcomeMessage from '../WelcomeMessage';
 
-const MyButton = styled.div`
-  color: green;
-  ${props =>
-    props.primary &&
-    css`
-      color: palevioletred;
-    `}
-`;
-
-const TomatoButton = styled(MyButton)`
-color: tomato;
-border-color: tomato;
-`
-
 function App () {
   return (
-    <>
+    <AppLayout>
       <WelcomeMessage />
-      <MyButton>Hi</MyButton>
-      <MyButton primary>Hello</MyButton>
-      <TomatoButton>Hello</TomatoButton>
-    </>
+    </AppLayout>
   );
 }
 
