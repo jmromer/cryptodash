3d3df634f01b15141682b0934be872c68d62a524
Jake Romer <mail@jakeromer.com>
2020-06-29 13:33:09 -0400

    Introduce styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './App.css';
+import styled, { css } from 'styled-components';
 
-import WelcomeMessage from '../WelcomeMessage'
+import WelcomeMessage from '../WelcomeMessage';
+
+const MyButton = styled.div`
+  color: green;
+  ${props =>
+    props.primary &&
+    css`
+      color: palevioletred;
+    `}
+`;
+
+const TomatoButton = styled(MyButton)`
+color: tomato;
+border-color: tomato;
+`
 
 function App () {
-  return <WelcomeMessage />;
+  return (
+    <>
+      <WelcomeMessage />
+      <MyButton>Hi</MyButton>
+      <MyButton primary>Hello</MyButton>
+      <TomatoButton>Hello</TomatoButton>
+    </>
+  );
 }
 
 export default App;
