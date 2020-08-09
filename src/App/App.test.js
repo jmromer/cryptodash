618947459b90ef592b389a6b92d6a9e75e8a6068
Jake Romer <mail@jakeromer.com>
2020-08-09 09:26:58 -0400

    Fix App component test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '.';
 
-test('renders learn react link', () => {
+test('renders CryptoDash dashboard', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Welcome to CryptoDash/i);
   expect(linkElement).toBeInTheDocument();
 });
